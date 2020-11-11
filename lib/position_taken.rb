92e25cd13870ae426a062bfca4c2cd43dc81386f
Matt Smith <mrandolph83@gmail.com>
2020-11-11 20:06:36 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@ def position_taken?(board, index)
 
   board[index]
 
-  if board[index] == " " || board[index] == "" || board[index] == nil
+  if board[index] == " " || board[index] == nil
    false
 
  else board[index] == "X" || board[index] == "O"
